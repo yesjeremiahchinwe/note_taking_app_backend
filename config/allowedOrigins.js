9eb5a4af3636f8c159a4bb586b7275621dab665b
Jeremiah Chinwe <chinwejeremiah9@gmail.com>
2025-08-13 23:56:30 +0100

    Fixes

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
-    'https://note-taking-app-frontend-pearl.vercel.app'
+    'https://note-taking-app-frontend-pearl.vercel.app',
+    'https://note-taking-app-frontend-5o60biyk8-yesjeremiahchinwes-projects.vercel.app'
 ]
 
 module.exports = allowedOrigins
