3c549e8092818fa3965323d1a0213c81dd270588
Jeremiah Chinwe <chinwejeremiah9@gmail.com>
2025-11-12 16:08:01 -0800

    Added a new allowed url

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'https://note-taking-app-frontend-pearl.vercel.app',
-    'https://note-taking-app-frontend-5o60biyk8-yesjeremiahchinwes-projects.vercel.app'
+    'https://note-taking-app-frontend-5o60biyk8-yesjeremiahchinwes-projects.vercel.app',
+    'https://note-taking-app-frontend-git-909c17-yesjeremiahchinwes-projects.vercel.app/'
 ]
 
 module.exports = allowedOrigins
