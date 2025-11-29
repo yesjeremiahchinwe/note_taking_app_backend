05db9d3f49b64c879e7a39107d555a6f51eb6382
Jeremiah Chinwe <chinwejeremiah9@gmail.com>
2025-11-29 12:53:44 -0800

    Added google auth and custom authentication with JWT token through out the application. Removed appwrite from the app

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,12 +1,34 @@
+const jwt = require("jsonwebtoken");
 
 const verifyJWT = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1] || req.headers.Authorization?.split(" ")[1];
   const authHeader = req.headers.authorization || req.headers.Authorization
 
-  if (!authHeader?.startsWith('Bearer ')) {
-      return res.status(401).json({ message: 'Unauthorized' })
-  }
+  if (!token || !authHeader?.startsWith('Bearer '))
+    return res.status(401).json({ message: "Unauthorized" });
 
-  next();
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
 };
 
 module.exports = verifyJWT;
+
+
+
+// const verifyJWT = (req, res, next) => {
+//   const authHeader = req.headers.authorization || req.headers.Authorization
+
+//   if (!authHeader?.startsWith('Bearer ')) {
+//       return res.status(401).json({ message: 'Unauthorized' })
+//   }
+
+//   next();
+// };
+
+// module.exports = verifyJWT;
+
